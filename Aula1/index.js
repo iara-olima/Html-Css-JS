e6f89a71d92a745780b3ce9caaa549fef7ef7fee
iara.lima <oiara2131.jipr@gmail.com>
2024-11-21 13:11:33 -0300

    Aulas iniciais e alguns testes

---
+++ Aula1/index.js
@@ -0,0 +1,18 @@
+'use scritct';
+
+const botao = document.querySelector('.btn');
+
+botao.addEventListener('click',()=>{
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+});
+
+//toggle remove se esta presente, adicona se nao esta - faz a troca
+
+//agora vai mudar a cor do botao conforme os cliques
+const nameClassAtualBotao = document.body.className;
+if(nameClassAtualBotao == 'light-theme'){
+    this.textContext = 'Dark';
+}else{
+    this.textContext = 'Light'
+}
