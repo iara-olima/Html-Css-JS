764f0e10510cf66fb84951d8bf70b9593d53ce7a
iara.lima <oiara2131.jipr@gmail.com>
2024-11-21 14:28:02 -0300

    salvando alterações

--- Aula1/index.js
+++ Aula1/index.js
@@ -5,14 +5,15 @@ const botao = document.querySelector('.btn');
 botao.addEventListener('click',()=>{
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
-});
+
 
 //toggle remove se esta presente, adicona se nao esta - faz a troca
 
 //agora vai mudar a cor do botao conforme os cliques
 const nameClassAtualBotao = document.body.className;
 if(nameClassAtualBotao == 'light-theme'){
-    this.textContext = 'Dark';
+    botao.textContent = 'Dark';
 }else{
-    this.textContext = 'Light'
+    botao.textContent = 'Light'
 }
+});
